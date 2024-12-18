1cf80054b0ac80d104c3a8d600ca798bbec9a984
卢晓锋 <lxfljw@gmail.com>
2024-12-18 22:41:27 +0800

    docs📝: 图片预览插件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,7 +3,11 @@ import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { onMounted } from "vue";
-import mediumZoom from "medium-zoom";
+// import mediumZoom from "medium-zoom";
+import "viewerjs/dist/viewer.min.css";
+import imageViewer from "vitepress-plugin-image-viewer";
+import vImageViewer from "vitepress-plugin-image-viewer/lib/vImageViewer.vue";
+import { useRoute } from "vitepress";
 import "./style.css";
 
 export default {
@@ -14,12 +18,19 @@ export default {
     });
   },
   enhanceApp({ app, router, siteData }) {
+    // 注册全局组件，如果你不想使用也可以不添加
+    app.component("vImageViewer", vImageViewer);
+    // ...
     // ...
   },
   setup() {
     onMounted(() => {
       // mediumZoom("[data-zoomable]", { background: "var(--vp-c-bg)" });
-      mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+      // mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+      // 获取路由
+      const route = useRoute();
+      // 使用
+      imageViewer(route);
     });
   },
 } satisfies Theme;
